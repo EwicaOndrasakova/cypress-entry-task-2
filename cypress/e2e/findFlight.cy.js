2e6883dbc5dae90300ddb184b4006c0296e97df4
Eva Ondrasakova <ewicaondrasakova@gmail.com>
2023-03-12 13:52:39 +0100

    Edits of tests

--- cypress/e2e/findFlight.cy.js
+++ cypress/e2e/findFlight.cy.js
@@ -1,5 +1,3 @@
-const button = '[type="button"]';
-
 describe("Test for Kiwi Cypress weekend", () => {
   beforeEach("Kiwi - main", () => {
     cy.setCookie("__kwc_agreed", "true");
@@ -7,61 +5,90 @@ describe("Test for Kiwi Cypress weekend", () => {
     cy.visit(
       "https://www.kiwi.com/en/airport/bcn/barcelona-el-prat-barcelona-spain/"
     );
+    cy.viewport(1200, 860);
   });
-  // });
-  //PREPISAT NAZVY V DESCRIPTION A NAZOV TESTu
-  //NAJST EXTENSIONS PRE UPRAVU KODU
+
   Cypress._.times(10, () => {
-    it("The flight to BCN is found", () => {
-      cy.get(
-        '[data-test="PlacePickerInput-destination"] > [data-test="SearchField-input"]'
-      ).click();
-      cy.get(
-        ".eXgNdq > .Box__StyledBox-sc-bvm5o6-0 > .Imagestyled__Img-sc-1hn1xfy-1"
-      ).should("be.visible");
+    it("The flight from Barcelona is found", () => {
+      //Skontroluj, že všetky sekcie a mapa sa zobrazia na stránke.
       cy.get("#sticky-search-form").should("be.visible");
-      cy.get('[data-test="SearchFieldItem-origin"]').should("be.visible");
+      cy.get(departureDestinationPicker).should("be.visible");
+      cy.get(arrivalDestinationPicker).should("be.visible");
+      cy.get(departureDestinationMapPreview).should("be.visible");
+      cy.get(trendingDestinationsList).should("be.visible");
 
-      cy.get('[data-test="PlacePickerInputPlace"]').contains("Barcelona BCN");
+      // Skontroluj, že v search form je origin správne nastavený na Barcelona BCN.
+      cy.get(searchFormOrigin).should("be.visible").contains("Barcelona BCN");
 
-      cy.get("h1").should("be.exist").contains("Barcelona–El Prat (BCN)");
-      cy.get('[data-test="PictureCard"]').eq("0").click();
+      //Skontroluj, že H1 element má správny text “Barcelona–El Prat (BCN)”.
+      cy.get("h1")
+        .should("be.exist")
+        .should("have.text", "Barcelona–El Prat (BCN)");
 
-      //6. Skontroluj, že si bol/a presmerovaný/á na stránku search/results so správne vyplneným search form. Ak nevieš, ako vyzerá, môžeš si to pozrieť tu.
-      //dorobit test
+      //V sekcii “ Popular destinations from Barcelona–El Prat (BCN)” klikni na prvú picture card.
+      cy.get(popularDesatinationsPictureCard).eq(0).click();
+
+      //Skontroluj, že si bol/a presmerovaný/á na stránku search/results so správne vyplneným search form.
       cy.url().should("include", "/search/results");
       cy.log("URL contains search/results");
-      // cy.get('[data-test="SearchForm-wrapper"]')
-      cy.get('[data-test="PlacePickerInputPlace"]').contains("Barcelona");
-      //tu doplnit ze from Barcelona, a kde doplnit nie konkretne miesto ale napriklad ze nie je prazdne ale vyplnene a pod.
 
-      cy.get('[data-test="PassengersField"]').click();
-      cy.get('[data-test="BagsPopup-cabin"]').find(button).eq("1").click();
+      //Pridaj vo filtroch jednu príručnú batožinu.
+      cy.get(passengersDropdown).click();
+      cy.get(cabinBaggageButton).find(button).eq(1).click();
+      cy.get(passengersFieldDoneButton).click();
 
-      cy.get(
-        ".ButtonWrapsstyled__ButtonTabletWrap-sc-zf781k-0 > .ButtonPrimitive__StyledButtonPrimitive-sc-j8pavp-0 > .ButtonPrimitiveContent__StyledButtonPrimitiveContent-sc-1nfggrh-0 > .ButtonPrimitiveContentChildren__StyledButtonPrimitiveContentChildren-sc-mra4yy-0 > :nth-child(2)"
-      ).contains("1");
-
-      cy.get('[data-test="ResultCardWrapper"]', { timeout: 15000 })
-        .eq("0")
+      //Presvedč sa, že sa ti načítali nové výsledky.
+      //Pokračuj na rezervačný formulár z prvého výsledku (klikni na tlačidlo "Select"/"Rezervovať").
+      cy.get(activeFiltersField).should("have.text", "1 filter active");
+      cy.get(resultCardPrice).should("be.visible");
+      cy.get(resultCard, { timeout: 15000 })
+        .eq(0)
         .should("be.visible")
-        .find('[data-test="BookingButton"]')
-        .contains("Select")
+        .find(bookingButton);
+
+      cy.get(bookingButton).contains("Select").click();
+
+      //Magic login
+      cy.get(loginModal)
+        .should("be.visible")
+        .find(continueAsGuestButton)
         .click();
 
-      cy.get('[data-test="MagicLogin-RequiredLogin"]')
-        .should("be.visible")
-        .find('[data-test="MagicLogin-GuestTextLink"]')
-        .contains("Continue as a guest")
-        .click();
+      // Over, že si sa dostal/a na booking stránku (rezervačný formulár).
+      cy.get(reservationContent).should("be.visible");
+      cy.get(reservationHead, { timeout: 15000 }).should("be.visible");
+      cy.get(reservationItinerary)
+        .find("h2")
+        .should("have.text", "Trip summary");
 
-      // 10. Over, že si sa dostal/a na booking stránku (rezervačný formulár).
-      cy.get('[data-test="ReservationHead"]', { timeout: 15000 })
-        //data-test="CommonJourneyHead"
-        .should("be.visible")
-        .find("h1")
-        .first()
-        .contains("Barcelona");
+      cy.get(reservationBillItems).should("be.visible");
+      cy.get(reservationBoxPrice).should("be.visible");
     });
   });
 });
+
+const button = '[type="button"]';
+const bookingButton = '[data-test="BookingButton"]';
+const departureDestinationPicker = '[data-test="PlacePickerInput-origin"]';
+const arrivalDestinationPicker = '[data-test="PlacePickerInput-destination"]';
+const departureDestinationMapPreview =
+  ".eXgNdq > .Box__StyledBox-sc-bvm5o6-0 > .Imagestyled__Img-sc-1hn1xfy-1";
+const trendingDestinationsList = '[data-test="TrendingDestinations"]';
+const searchFormOrigin = '[data-test="SearchFieldItem-origin"]';
+const popularDesatinationsPictureCard = '[data-test="PictureCard"]';
+const passengersDropdown = '[data-test="PassengersField"]';
+const cabinBaggageButton =
+  ':nth-child(2) > .BagsPickerstyled__Wrapper-sc-h2j3jl-0 > [data-test="BagsPopup-cabin"]';
+const passengersFieldDoneButton = '[data-test="PassengersFieldFooter-done"]';
+const resultCard = '[data-test="ResultCardWrapper"]';
+const activeFiltersField =
+  ".FiltersSidebarstyled__CountAlert-sc-k6gw0b-1 > .Text__StyledText-sc-1dj99rd-0";
+const resultCardPrice = '[data-test="ResultCardPrice"]';
+const loginModal = '[data-test="MagicLogin-RequiredLogin"]';
+const continueAsGuestButton = '[data-test="MagicLogin-GuestTextLink"]';
+const reservationItinerary =
+  '[data-test="ReservationItinerary"] > .cveCbd > .SDOJk > .jerigJ > .Stack__StyledStack-sc-53pobq-0';
+const reservationBoxPrice = '[data-test="ReservationBillBoxItemPrice"]';
+const reservationContent = '[data-test="Reservation-content"]';
+const reservationHead = '[data-test="ReservationHead"]';
+const reservationBillItems = ".ReservationBill-items-wrapper";
